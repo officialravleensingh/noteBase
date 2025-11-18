99fa271af6d2f232595b6f815996f6ccc45d234f
Ravleen Singh <officialravleensingh@gmail.com>
2025-11-18 17:20:16 +0530

    Done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
+const cors = require('cors');
 
 const app = require('./src/app');
 const { connectDB } = require('./config/database');
 
+app.use(cors({
+  origin : "*",
+  credentials : true
+}));
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
